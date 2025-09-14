cc8f3c1826929890b2c687b02b59ab3260be5290
Hansini <hansinig163@gmail.com>
2025-09-14 20:08:30 +0530

    first commit

---
+++ menu.py
@@ -0,0 +1,8 @@
+import numpy as np
+def menu():
+    print("Daily planner")
+    print("1. Add a task")
+    print("2. View tasks")
+    print("3. delete")
+    print("4. Exit")
+    print("5. view calendar")
